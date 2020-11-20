bcb7cc87820211d577590526f5317004c67093f1
mohammedrazakhan <rk6265766@gmail.com>
2020-11-20 21:10:33 +0530

    Update auth.dart

--- lib/Backend/auth.dart
+++ lib/Backend/auth.dart
@@ -1,8 +1,7 @@
-import 'dart:html';
+
 
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_auth_web/firebase_auth_web.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;

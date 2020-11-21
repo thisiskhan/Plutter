acf3b901ff4023a9dd19b4e210b35d75ce0486c3
mohammedrazakhan <rk6265766@gmail.com>
2020-11-21 14:42:33 +0530

    Update auth.dart

--- lib/Backend/auth.dart
+++ lib/Backend/auth.dart
@@ -15,7 +15,7 @@ class AuthService {
       if (e.code == 'user-not-found') {
         print('No user found for that email.');
       } else if (e.code == 'wrong-password') {
-        print('Wrong password provided for that user.');
+        print('Wrong password provided for that username ');
       }
     }
   }
